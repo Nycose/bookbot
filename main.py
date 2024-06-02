b6c9c5cf26ff0f7110f78f98d836edf40e5df054
Mason Crawford <mcrawford.personal@gmail.com>
2024-06-02 14:33:36 -0500

    count words

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+def count_words(words):
+    return len(words.split())
+
 def main():
     with open('books/frankenstein.txt') as f:
         file_contents = f.read() 
-        print(file_contents)
+        # print(file_contents)
+        print(f"word count: {count_words(file_contents)}")
 
 main()
